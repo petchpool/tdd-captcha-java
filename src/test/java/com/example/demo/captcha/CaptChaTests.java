93d283f7d892245825863151e2c055d26f5e6902
Wisit Phusi <wisit.phusi@gmail.com>
2019-08-06 16:18:59 +0700

    Updated

--- src/test/java/com/example/demo/captcha/CaptChaTests.java
+++ src/test/java/com/example/demo/captcha/CaptChaTests.java
@@ -1,9 +1,11 @@
 package com.example.demo.captcha;
 
 import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.Assert.assertEquals;
 
+@SpringBootTest
 public class CaptChaTests {
 
     @Test
